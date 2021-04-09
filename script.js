114cfcce6983934746f97f16c200ffebf067dcfa
jbpixel52 <50649423+jbpixel52@users.noreply.github.com>
2021-04-08 20:42:27 -0700

    momento PUSH

--- script.js
+++ script.js
@@ -1,92 +1,130 @@
-
 var renderer, scene, camera, model;
 
-var xRotate=0.01;
-var yRotate=0;
-var zRotate=0;
+var xRotate = 0;
+var yRotate = 0;
+var zRotate = 0;
 
-var xTranslate=0;
-var yTranslate=0;
-var zTranslate=0;
+var xTranslate = 0;
+var yTranslate = 0;
+var zTranslate = 0;
 
-var xScale=0;
-var yScale=0;
-var zScale=0;
+var xScale = 0;
+var yScale = 0;
+var zScale = 0;
 
 var ww = window.innerWidth,
-	wh = window.innerHeight;
+    wh = window.innerHeight;
 
-function init(){
-  
-  document.getElementById("xTranslation").onchange=function(){
-  xTranslate = parseFloat(event.target.value);
-  console.log(xT);};
-document.getElementById("yTranslation").onchange=function(){
-  yTranslate = parseFloat(event.target.value);};
-document.getElementById("zTranslation").onchange=function(){
-  zTranslate = parseFloat(event.target.value);};
+function init() {
 
-	renderer = new THREE.WebGLRenderer({canvas : document.getElementById('gl-canvas')});
-	renderer.setSize(ww,wh);
+    document.getElementById("xTranslation").onchange = function () {
+        xTranslate = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+        console.log('xTranslate: ' + xTranslate);
+        init();
+    };
+    document.getElementById("yTranslation").onchange = function () {
+        yTranslate = parseFloat(event.target.value);
+        console.log('yTranslate: ' + yTranslate);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("xScaling").onchange = function () {
+        xScale = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("yScaling").onchange = function () {
+        yScale = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("yScaling").onchange = function () {
+        zScale = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("xRotation").onchange = function () {
+        xRotate = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("yRotation").onchange = function () {
+        yRotate = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    document.getElementById("zRotation").onchange = function () {
+        zRotate = parseFloat(event.target.value);
+        requestAnimationFrame(render);
+    };
+    renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById('gl-canvas')
+    });
+    renderer.setSize(ww, wh);
 
-	scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera(50,ww/wh, 0.1, 10000 );
-	camera.position.set(0,0,500);
-	scene.add(camera);
+    camera = new THREE.PerspectiveCamera(50, ww / wh, 0.1, 10000);
+    camera.position.set(0, 0, 500);
+    scene.add(camera);
 
-	//Add a light in the scene
-	directionalLight = new THREE.DirectionalLight( 0xffffff, 0.8 );
-	directionalLight.position.set( 0, 0, 350 );
-	directionalLight.lookAt(new THREE.Vector3(0,0,0));
-	scene.add( directionalLight );
+    //Add a light in the scene
+    directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+    directionalLight.position.set(0, 0, 350);
+    directionalLight.lookAt(new THREE.Vector3(0, 0, 0));
+    scene.add(directionalLight);
 
-	//Load the obj file
-	loadOBJ();
+    //Load the obj file
+
+    loadOBJ();    
+    requestAnimationFrame(render);
+
 }
 
-var loadOBJ = function(){
+var loadOBJ = function () {
 
-	//Manager from ThreeJs to track a loader and its status
-	var manager = new THREE.LoadingManager();
-	//Loader for Obj from Three.js
-	var loader = new THREE.OBJLoader( manager );
-  
-	//Launch loading of the obj file, addBananaInScene is the callback when it's ready 
-	loader.load( 'https://raw.githubusercontent.com/Alecfut07/Graficas-y-visualizacion/master/Casquito.obj', addModeltoScene);
+    //Manager from ThreeJs to track a loader and its status
+    var manager = new THREE.LoadingManager();
+    //Loader for Obj from Three.js
+    var loader = new THREE.OBJLoader(manager);
+
+    //Launch loading of the obj file, addBananaInScene is the callback when it's ready 
+    loader.load('https://raw.githubusercontent.com/Alecfut07/Graficas-y-visualizacion/master/Casquito.obj', addModeltoScene);
 
 };
 
-var addModeltoScene = function(object){
-	model = object;  
-	model.position.x = xTranslate;
-	model.position.y = yTranslate;
-	model.position.z = zTranslate;
-    model.scale.x=2+xScale;
-    model.scale.y=2+yScale;
-    model.scale.z=2+zScale;
-    
+var addModeltoScene = function (object) {
+    model = object;
+    model.needsUpdate=true;
 
-  
-	object.traverse( function ( child ) {
-		if(child instanceof THREE.Mesh){
-			child.material.color = new THREE.Color('pink');
-			child.geometry.computeVertexNormals();
-		}
-	});
-	//aqui se agrega el modelo a la escena
-	scene.add(model);
-	render();
+    //model.position.x = xTranslate;
+    //model.position.y = yTranslate;
+    //model.position.z = zTranslate;
+    //model.TranslateX(xTranslate);
+    //model.TranslateY(yTranslate);
+    //model.TranslateZ(zTranslate);
+    model.scale.x = 2 + xScale;
+    model.scale.y = 2 + yScale;
+    model.scale.z = 2 + zScale;
+
+
+
+
+    object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.material.color = new THREE.Color('pink');
+            child.material.needsUpdate = true;
+            child.geometry.computeVertexNormals();
+        }
+    });
+    //aqui se agrega el modelo a la escena
+    scene.add(model);
+    render();
 };
 
 
 var render = function () {
-	requestAnimationFrame(render);
-	model.rotation.x += xRotate;
-	model.rotation.y += yRotate;
-	model.rotation.z += zRotate;
+    requestAnimationFrame(render);
+    model.rotation.x += xRotate;
+    model.rotation.y += yRotate;
+    model.rotation.z += zRotate;
 
-	renderer.render(scene, camera);
+    renderer.render(scene, camera);
 };
 
 init();
